e8a6e3a5e4cae478a5d248cb61ec837d52660290
Aliyu <88856370+AllyCodeX@users.noreply.github.com>
2022-07-19 07:29:08 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,13 +22,13 @@ function App() {
   };
 
   const firebaseConfig = {
-    apiKey: "AIzaSyBNN1t82sFc-CWUyyW8Mpsmo4zQnqot63s",
-    authDomain: "dma-5c98f.firebaseapp.com",
-    projectId: "dma-5c98f",
-    storageBucket: "dma-5c98f.appspot.com",
-    messagingSenderId: "601101583020",
-    appId: "1:601101583020:web:08fcb8e80fa3e898fe9509",
-    measurementId: "G-8T01HY2VVN",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
 
   };
   if (!firebase.apps.length) {
